46317d8279e46ad221c399cfb44b12d785c6a0ef
leeexiu <leeexiu@gmail.com>
2022-07-04 19:57:23 +0900

    19:57 초안

---
+++ account_book.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#한 사람의 통장을 받아서 정리할 목적으로 구현되는 클래스
+class account_book():
+  
+    def UserInput():
+        user = input("통장을 삽입하세요.")
+        return user
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
