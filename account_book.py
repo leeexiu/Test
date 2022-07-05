8208739b445ab4e6da2137a7352b79ffc8db8ec9
leeexiu <leeexiu@gmail.com>
2022-07-05 10:58:57 +0900

    10:58

--- account_book.py
+++ account_book.py
@@ -6,34 +6,28 @@
 
 #한 사람의 통장을 받아서 정리할 목적으로 구현되는 클래스
 class account_book():
-  
-    def UserInput(): # 통장삽입 
+    #수정
+    def UserInput(): # 통장삽입
         user = input("통장을 삽입하세요.")
         return user
-    
+
     def Useraccount_book(user): # 통장삽입 실행
         print('-' *30)
         print(user + "을(를) 삽입하셨습니다. 통장정리 버튼을 눌러주세요")
         print('-' *30)
-        
+
         return int(input())
-    
+
     def Useraccount_book_start(user): # 통장정리 실행
         print('-' *30)
         print(user + "정리 중입니다. 잠시만 기다려 주십시오.")
         print('-' *30)
-    
+
     while()
         if int(input("정리가 완료되었습니다. 투입구에서 통장을 빼주세요.") == 1:
-          
+
         else :
             print("안녕히가십시오")
-       
-
-
-# In[ ]:
-
-
 
 
 
@@ -54,3 +48,5 @@ class account_book():
 
 
 
+
+# In[ ]:
